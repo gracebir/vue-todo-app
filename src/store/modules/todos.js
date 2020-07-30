07ea740432fcd68ed2bf141f3927c14d8e7ffba1
funcoder <grace.devolop@gmail.com>
2020-07-30 08:05:26 +0200

    get todos

---
+++ src/store/modules/todos.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const state = {
+    todos:[]
+};
+
+const getters = {
+    allTodos : (state) => state.todos
+}
+
+const actions = {
+    async fetchTodos({ commit }){
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        commit('setTodos', response.data)
+    },
+    // method of adding todos
+    async addTodo({ commit }, title){
+        const response = await axios.post('https://jsonplaceholder.typicode.com/todos',{title, completed:false });
+        commit('newTodo', response.data);
+    },
+}
+
+const mutations = {
+    setTodos : (state, todos) => (state.todos = todos),
+    addTodo: (state, todo) => state.todos.unshift(todo),
+}
+
+export default{
+    state,
+    getters,
+    actions,
+    mutations
+}
