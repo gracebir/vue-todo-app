2cd8c2953221cb7e9159d9f36d8eb587db31fd77
funcoder <grace.devolop@gmail.com>
2020-07-30 09:49:26 +0200

    update todos if completed

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,27 +1,40 @@
 <template>
-    <div>
-        <h3>Todo</h3>
-        <div class="todos">
-            <div v-for="todo in allTodos" :key="todo.id" class="todo">
-                {{todo.title}}
-                <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
-            </div>
-        </div>
+  <div>
+    <h3>Todo</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span> <span class="incomplete-box"></span> = Incomplete </span>
+      <span> <span class="complete-box"></span> = Complete </span>
     </div>
+    <div class="todos">
+      <div @dblclick="onDblClick(todo)" v-for="todo in allTodos" :key="todo.id" v-bind:class="{'is-complete': todo.completed}" class="todo">
+        {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from "vuex";
 export default {
-    name: "Todo",
-    computed: mapGetters(['allTodos']),
-    methods: {
-        ...mapActions(['fetchTodos','deleteTodo']),
-    },
-    created(){
-        this.fetchTodos();
+  name: "Todo",
+  computed: mapGetters(["allTodos"]),
+  methods: {
+    ...mapActions(["fetchTodos", "deleteTodo","updateTodo"]),
+    onDblClick(todo){
+        const updTodo = {
+            id: todo.id,
+            title : todo.title,
+            completed : !todo.completed
+        }
+        this.updateTodo(updTodo);
     }
-    }
+  },
+  created() {
+    this.fetchTodos();
+  },
+};
 </script>
 
 <style scoped>
